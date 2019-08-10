1c9d2d69c5408e0adfb46fc97f93151176cb8703
Hugh Guiney <hugh@hughguiney.com>
2019-08-10 04:00:04 -0400

    Working Strapi integration

---
+++ .eslintrc.js
@@ -0,0 +1,16 @@
+module.exports = {
+  "extends": [
+    "hughx"
+  ],
+  "plugins": [
+    "react"
+  ],
+  "parserOptions": {
+    "ecmaFeatures": {
+      "jsx": true
+    }
+  },
+  "rules": {
+    "quotes": ["error", "double"],
+  }
+};
--- gatsby-node.js
+++ gatsby-node.js
@@ -5,3 +5,28 @@
  */
 
 // You can delete this file if you're not using it
+
+// https://github.com/strapi/gatsby-source-strapi/issues/8#issuecomment-434269925
+const { createRemoteFileNode } = require( "gatsby-source-filesystem" );
+
+exports.onCreateNode = async ( {
+  node, actions, store, cache,
+} ) => {
+  const { createNode, createNodeField } = actions;
+
+  if ( node.internal.type !== null && node.internal.type === "StrapiPost" ) {
+    for ( const image of node.coverArt ) {
+      const fileNode = await createRemoteFileNode( { // eslint-disable-line
+        "url": `http://localhost:1337${image.url}`,
+        store,
+        cache,
+        createNode,
+        "createNodeId": id => image.id,
+      } );
+
+      if ( fileNode ) {
+        image.localFile___NODE = fileNode.id;
+      }
+    }
+  }
+};
