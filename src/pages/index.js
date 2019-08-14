729401b053c9a693bc677d43a8557b32d7ae5fda
Hugh Guiney <hugh@hughguiney.com>
2019-08-13 23:14:51 -0400

    Basic blog post feed

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,29 +3,23 @@ import { Link, graphql } from "gatsby";
 import Img from "gatsby-image";
 
 import Layout from "../components/layout";
-import Image from "../components/image";
 import SEO from "../components/seo";
 
 const IndexPage = ( { data } ) => (
   <Layout>
-    <SEO title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <div style={{ "maxWidth": "300px", "marginBottom": "1.45rem" }}>
-      <Image />
-    </div>
-    <div>
-      {data.allStrapiPost.edges.map( post => (
+    <SEO title="Hugh Guiney – UX Designer, Web Developer" />
+    <article>
+      <h2>Recent Blog Posts</h2>
+      { data.allStrapiPost.edges.map( post => (
         <article key={post.node.id}>
           <h2>
             <Link to={`/${post.node.id}`}>{post.node.title}</Link>
           </h2>
-          <Img fixed={post.node.coverArt[0].localFile.childImageSharp.fixed}/>
-          <p>{post.node.body}</p>
+          <Img fluid={post.node.coverArt[0].localFile.childImageSharp.fluid}/>
+          <p>{ post.node.body.split( "\n\n" )[0] }</p>
         </article>
-      ) )}
-    </div>
+      ) ) }
+    </article>
     <Link to="/page-2/">Go to page 2</Link>
   </Layout>
 );
@@ -34,7 +28,13 @@ export default IndexPage;
 
 export const pageQuery = graphql`
 query IndexQuery {
-  allStrapiPost {
+  allStrapiPost(
+    sort: {
+      fields: [createdAt],
+      order: DESC
+    },
+    limit: 4
+  ) {
     edges {
       node {
         id
@@ -45,8 +45,8 @@ query IndexQuery {
           mime
           localFile {
             childImageSharp {
-              fixed(width: 200, height: 125) {
-                ...GatsbyImageSharpFixed
+              fluid(maxWidth: 1200, maxHeight: 630) {
+                ...GatsbyImageSharpFluid
               }
             }
           }
@@ -56,5 +56,4 @@ query IndexQuery {
     }
   }
 }
-
 `;
