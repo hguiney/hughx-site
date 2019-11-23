2c732106947442764520c1b4bafc74d8d59648e8
Hugh Guiney <hugh@hughguiney.com>
2019-11-23 17:09:57 -0500

    Make blog post images less imposing

--- src/components/blog-preview.js
+++ src/components/blog-preview.js
@@ -1,17 +1,23 @@
 import React from "react";
 import PropTypes from "prop-types";
 import { Link, graphql } from "gatsby";
+import styled from "styled-components";
 
 import PostPreview from "./post-preview";
 
 const BlogPreview = ( props ) => {
   const { posts } = props;
+  const Article = styled.article`
+    max-width: 43rem;
+    margin-left: auto;
+    margin-right: auto;
+  `;
 
   return (
-    <article id="blog">
+    <Article id="blog">
       <h2>From the <Link to="/blog/">Blog</Link>:</h2>
       { posts.edges.map( edge => <PostPreview key={ edge.node.id } post={ edge.node } /> ) }
-    </article>
+    </Article>
   );
 };
 
