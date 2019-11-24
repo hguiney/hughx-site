fdce6a85b6b8d9aa56d7c8480390c2288fb73504
Hugh Guiney <hugh@hughguiney.com>
2019-11-23 19:12:41 -0500

    Make svg2png work regardless of logo status

--- svg2png.js
+++ svg2png.js
@@ -3,18 +3,16 @@ const fs = require( "fs" );
 const path = require( "path" );
 const { exec } = require( "child_process" );
 const glob = require( "glob" );
+const mkdirp = require( "mkdirp" );
+const rimraf = require( "rimraf" );
 
-const logosDirectory = path.resolve( __dirname, "static/images/logos" );
-const logosSearch = `${logosDirectory}/*.svg`;
-const outputDirectory = `${logosDirectory}/rasterized`;
+const imagesDirectory = path.resolve( __dirname, "static/images" );
+const imagesSearch = `${imagesDirectory}/**/*.svg`;
+const rasterizedDirectory = `${imagesDirectory}/rasterized`;
 
-const toDelete = glob.sync( `${outputDirectory}/*.png` );
+rimraf.sync( `${rasterizedDirectory}/**/*` );
 
-toDelete.forEach( ( bitmap ) => {
-  fs.unlinkSync( bitmap );
-} );
-
-glob( logosSearch, ( error, files ) => {
+glob( imagesSearch, ( error, files ) => {
   if ( error ) {
     console.error( "Could not list the directory.", error );
     process.exit( 1 );
@@ -28,12 +26,16 @@ glob( logosSearch, ( error, files ) => {
         density = `@${index + 1}x`;
       }
 
-      const outfile = `${outputDirectory}/${path.basename( file ).replace( ".svg", `${density}.png` )}`;
+      const subpath = path.dirname( file ).replace( new RegExp( `^${imagesDirectory.replace( "/", "\\/" )}` ), "" );
+      const outputDirectory = `${rasterizedDirectory}/${subpath}`;
+      const outfile = `${outputDirectory}/${path.basename( file ).replace( ".svg", `${density}.png` )}`; // .replace('/', path.sep);
       const params = [
         "-z", "-e", `'${outfile}'`,
         "-w", width.toString(),
         `'${file}'`,
       ];
+
+      mkdirp.sync( outputDirectory );
 
       exec( `inkscape ${params.join( " " )}`, ( inkscapeError, stdout, stderr ) => {
         if ( inkscapeError ) {
