5693afca6ce8c1a500d84e4ef57b7617af8d7fa6
Hugh Guiney <hugh@hughguiney.com>
2019-12-05 19:20:54 -0500

    Update dependencies

--- gatsby-config.js
+++ gatsby-config.js
@@ -15,7 +15,7 @@ module.exports = {
       },
     },
     {
-      "resolve": "gatsby-source-strapi",
+      "resolve": "gatsby-source-strapi-multiple-images",
       "options": {
         "apiURL": "http://localhost:1337",
         "contentTypes": [ // List of the Content Types you want to be able to request from Gatsby.
