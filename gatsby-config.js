869771a951edc0e3530415e3ab5206d84ca18beb
Hugh Guiney <hugh@hughguiney.com>
2019-12-15 20:38:18 -0500

    Remove Stripe for now

--- gatsby-config.js
+++ gatsby-config.js
@@ -43,7 +43,7 @@ module.exports = {
     },
     "gatsby-plugin-offline",
     "gatsby-plugin-styled-components",
-    "gatsby-plugin-stripe",
+    // "gatsby-plugin-stripe",
     // {
     //   "resolve": "gatsby-plugin-stripe",
     //   "options": {
