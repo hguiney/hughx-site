b5ac4b8f6f71ea32d59978854dad290070564431
Hugh Guiney <hugh@hughguiney.com>
2019-12-15 20:39:49 -0500

    Develop inline SVG capability for ProgressiveImage

--- src/components/advantages.js
+++ src/components/advantages.js
@@ -2,6 +2,7 @@ import React from "react";
 // import PropTypes from "prop-types";
 import styled from "styled-components";
 
+import Img from "gatsby-image";
 import FinePrint from "./fine-print";
 import ProgressiveImage from "./progressive-image";
 
@@ -20,10 +21,10 @@ const Article = styled.article`
     // margin-left: auto;
     // margin-right: auto;
 
-    & > section {
+    & > div > section {
       // max-width: 39rem;
 
-      &:nth-child(odd) {
+      &:nth-child(even) {
         // margin-left: auto;
         flex-direction: row-reverse;
 
@@ -46,7 +47,7 @@ const Section = styled.section`
     margin-right: auto;
   }
 
-  .two-column > & {
+  .two-column & {
     @media only screen and (min-width: 85em) {
       flex-direction: row;
   
@@ -73,8 +74,10 @@ const AdvantageHeading = styled.h3`
   hyphens: auto;
   text-align: center;
   font-weight: 900;
+  -webkit-background-clip: text;
+  background-clip: text;
 
-  &.two-columns > & {
+  .two-column & {
     @media only screen and (min-width: 85em) {
       text-align: left;
     }
@@ -95,60 +98,88 @@ const AdvantagesHeading = styled.h2`
   margin-bottom: 3rem;
 `;
 
+const AdvantagesBody = styled.div``;
+
 const Advantages = () => (
   <Article
     id="advantages"
     // className="two-column"
   >
     <AdvantagesHeading>The hughx Advantage:</AdvantagesHeading>
-    <Section>
-      <ProgressiveImage
-        img={ {
-          "src": piggyBankImage,
-          "width": advantageImageWidth,
-          "alt": "icon: piggy bank with coins entering",
-        } }
-        style={ advantageImageStyle }
-      />
-      <div>
-        <AdvantageHeading>Economical.</AdvantageHeading>
-        { /* <p>With traditional app development, the designer spends untold hours on a product mockup. Then the developer has to spend even more hours making it real. The developer often struggles to realize the designer’s vision under real-world constraints. The result not only costs double or triple what it should have, but it increases time-to-market.</p> */ }
-        <p>With traditional app development, there are two main phases: design and development.</p>
-        <p>First, the designer creates mockups—a series of pictures of what the app will look like. Then the developer translates those pictures into actual code. This duplicates the work of the designer, costing you time and money. Often the designer lacks technical knowledge. At best, this makes the developer’s job harder. At worst this means design revisions. Either way it means more time and more money.</p>
-        <p>There’s a better way. As a hybrid designer–developer, I don’t do mockups—only functional prototypes. That way you can <em>feel</em> what the product will be like, rather than just imagine it. And with fewer cooks in the kitchen, you get to launch and iterate faster.</p>
-      </div>
-    </Section>
-    <Section>
-      <ProgressiveImage
-        img={ {
-          "src": empathyImage,
-          "width": advantageImageWidth,
-          "alt": "icon: face with human figure inside",
-        } }
-        style={ advantageImageStyle }
-      />
-      <div>
-        <AdvantageHeading>Empathetic.</AdvantageHeading>
-        <p>50% of the world’s population isn’t online yet<a id="citation-1" href="#footnote-1"><sup>1</sup></a>, but they soon will be. As this happens, they will largely arrive by way of mobile, not desktop. And not on the iPhone 11, either—more like the iPhone from 2011. This means sites and apps designed for Westerners with the latest tech will break for the rest of the world.</p>
-        <p>I believe the Internet should be for everyone. I keep accessibility, performance, and user friendliness top-of-mind at every stage of development. This helps your product reach the broadest possible audience. It’s not just good karma, it’s good business.</p>
-      </div>
-    </Section>
-    <Section>
-      { /* <h3>Built to last.</h3> */ }
-      <ProgressiveImage
-        img={ {
-          "src": blueprintsImage,
-          "width": advantageImageWidth,
-          "alt": "icon: blueprints",
-        } }
-        style={ advantageImageStyle }
-      />
-      <div>
-        <AdvantageHeading>Engineered.</AdvantageHeading>
-        <p>Just because you’re moving fast doesn’t mean you need to sacrifice quality. Today’s MVP is tomorrow’s legacy codebase. Left unpaid, technical debt manifests as bottlenecks. New features start to break old ones. The app slows to a crawl, increasing bounce rate and costing you sales. Current employees get pulled away from product work to help onboard new employees.</p>
-        <p>Contracting with hughx is an insurance policy against code rot. Modular functions, unit tests, and detailed documentation all come standard. If your customers don’t thank you, your dev team will.</p>
-      </div>
-    </Section>
+    <AdvantagesBody>
+      <Section style={ {
+        // "--color-stop-1": "goldenrod",
+        // "--color-stop-1": "tomato",
+        // "--color-stop-2": "#fdb99b",
+        // "--color-stop-3": "tomato",
+      } }>
+        <ProgressiveImage
+          img={ {
+            "src": piggyBankImage,
+            "width": advantageImageWidth,
+            "viewBox": "0 0 100.0002 100.0004",
+            "alt": "icon: piggy bank with coins entering",
+            // "style": {
+            //   "fill": "url(#piggy-bank-gradient) black",
+            // },
+          } }
+          style={ {
+            ...advantageImageStyle,
+          } }
+          inlineSvg
+        >
+          { /* <linearGradient id="piggy-bank-gradient" x2="0" y2="1">
+            <stop offset="0%" stopColor="var(--color-stop-1)"></stop>
+            <stop offset="90%" stopColor="var(--color-stop-2)"></stop>
+          </linearGradient> */ }
+        </ProgressiveImage>
+        <div>
+          <AdvantageHeading style={ {
+            // "color": "transparent",
+            // "backgroundImage": "linear-gradient(180deg, var(--color-stop-2) 0%, var(--color-stop-2) 50%, var(--color-stop-3) 100%)",
+          } }>Economical.</AdvantageHeading>
+          { /* <p>With traditional app development, the designer spends untold hours on a product mockup. Then the developer has to spend even more hours making it real. The developer often struggles to realize the designer’s vision under real-world constraints. The result not only costs double or triple what it should have, but it increases time-to-market.</p> */ }
+          <p>With traditional app development, there are two main phases: design and development.</p>
+          <p>First, the designer creates mockups—a series of pictures of what the app will look like. Then the developer translates those pictures into actual code. This duplicates the work of the designer, costing you time and money. Often the designer lacks technical knowledge. At best, this makes the developer’s job harder. At worst this means design revisions. Either way it means more time and more money.</p>
+          <p>There’s a better way. As a hybrid designer–developer, I don’t do mockups—only functional prototypes. That way you can <em>feel</em> what the product will be like, rather than just imagine it. And with fewer cooks in the kitchen, you get to launch and iterate faster.</p>
+        </div>
+      </Section>
+      <Section>
+        <ProgressiveImage
+          img={ {
+            "src": empathyImage,
+            "width": advantageImageWidth,
+            "viewBox": "0 0 423.7782 514.8713",
+            "alt": "icon: face with human figure inside",
+          } }
+          style={ advantageImageStyle }
+          inlineSvg
+        />
+        <div>
+          <AdvantageHeading>Empathetic.</AdvantageHeading>
+          <p>50% of the world’s population isn’t online yet<a id="citation-1" href="#footnote-1"><sup>1</sup></a>, but they soon will be. As this happens, they will largely arrive by way of mobile, not desktop. And not on the iPhone 11, either—more like the iPhone from 2011. This means sites and apps designed for Westerners with the latest tech will break for the rest of the world.</p>
+          <p>I believe the Internet should be for everyone. I keep accessibility, performance, and user friendliness top-of-mind at every stage of development. This helps your product reach the broadest possible audience. It’s not just good karma, it’s good business.</p>
+        </div>
+      </Section>
+      <Section>
+        { /* <h3>Built to last.</h3> */ }
+        <ProgressiveImage
+          img={ {
+            "src": blueprintsImage,
+            "width": advantageImageWidth,
+            "viewBox": "0 0 60 60",
+            "alt": "icon: blueprints",
+          } }
+          style={ advantageImageStyle }
+          inlineSvg
+        />
+        <div>
+          <AdvantageHeading>Engineered.</AdvantageHeading>
+          <p>Just because you’re moving fast doesn’t mean you need to sacrifice quality. Today’s MVP is tomorrow’s legacy codebase. Left unpaid, technical debt manifests as bottlenecks. New features start to break old ones. The app slows to a crawl, increasing bounce rate and costing you sales. Current employees get pulled away from product work to help onboard new employees.</p>
+          <p>Contracting with hughx is an insurance policy against code rot. Modular functions, unit tests, and detailed documentation all come standard. If your customers don’t thank you, your dev team will.</p>
+        </div>
+      </Section>
+    </AdvantagesBody>
     <FinePrint>
       <small>
         <p id="footnote-1"><sup>1</sup> Source: <a href="https://www.itu.int/en/ITU-D/Statistics/Pages/stat/default.aspx"><cite>Statistics</cite> on ITU</a> <a href="#citation-1" title="Back to citation marker">↩</a></p>
--- src/components/progressive-image.js
+++ src/components/progressive-image.js
@@ -26,7 +26,7 @@ const getSrc = ( logo ) => {
   return logo.src;
 };
 
-const isBase64 = src => ( /^data:image\/[^;]+;base64,/.test( src ) );
+const isBase64 = ( src ) => ( /^data:image\/[^;]+;base64,/.test( src ) );
 
 const getSrcSet = ( logo ) => {
   if ( isBase64( logo.src ) ) {
@@ -58,28 +58,68 @@ const getSrcSet = ( logo ) => {
   return srcset;
 };
 
-const ProgressiveImage = ( { className, img, style } ) => (
-  <picture className={ className } style={ {
-    "lineHeight": 0,
-    "display": "inline-flex",
-    "justifyContent": "flex-start",
-    ...style,
-  } }>
-    {
-      ( !isBase64( img.src ) && isSvg( img.src ) )
-        ? <source type="image/svg+xml" srcSet={ img.src } />
-        : null
-    }
-    <img
-      width={ img.width }
-      height={ img.height }
-      src={ getSrc( img ) }
-      srcSet={ getSrcSet( img ) }
-      alt={ img.alt }
-      style={ img.style }
-    />
-  </picture>
-);
+const ProgressiveImage = ( {
+  className, img, style, inlineSvg, children,
+} ) => {
+  if ( !isBase64( img.src ) && isSvg( img.src ) && inlineSvg ) {
+    return (
+      <picture className={ className } style={ {
+        "lineHeight": 0,
+        "display": "inline-flex",
+        "justifyContent": "flex-start",
+        ...style,
+      } }>
+        <svg
+          viewBox={ img.viewBox || ( img.width ? `0 0 ${img.width} ${img.height || img.width}` : null ) }
+          width={ img.width }
+          height={ img.height }
+          style={ img.style }
+        >
+          <defs>
+            { children }
+          </defs>
+          <desc>{ img.alt }</desc>
+          <switch>
+            <use xlinkHref={ `${img.src}#icon` } />
+            <foreignObject>
+              <img
+                width={ img.width }
+                height={ img.height }
+                src={ getSrc( img ) }
+                srcSet={ getSrcSet( img ) }
+                alt={ img.alt }
+                style={ img.style }
+              />
+            </foreignObject>
+          </switch>
+        </svg>
+      </picture>
+    );
+  }
+
+  return (
+    <picture className={ className } style={ {
+      "lineHeight": 0,
+      "display": "inline-flex",
+      "justifyContent": "flex-start",
+      ...style,
+    } }>
+      {
+        ( !isBase64( img.src ) && isSvg( img.src ) )
+          ? <source type="image/svg+xml" srcSet={ img.src } />
+          : null
+      }
+      <img
+        width={ img.width }
+        height={ img.height }
+        src={ getSrc( img ) }
+        srcSet={ getSrcSet( img ) }
+        alt={ img.alt }
+        style={ img.style }
+      />
+    </picture>
+  );
+};
 
 ProgressiveImage.propTypes = {
   "className": PropTypes.oneOfType( [PropTypes.string, PropTypes.object] ),
@@ -87,10 +127,12 @@ ProgressiveImage.propTypes = {
     "id": PropTypes.string,
     "width": isNumeric,
     "height": isNumeric,
+    "viewBox": PropTypes.string,
     "src": PropTypes.string,
     "alt": PropTypes.string.isRequired,
     "style": PropTypes.object,
   } ),
+  "inlineSvg": PropTypes.bool,
 };
 
 export default ProgressiveImage;
