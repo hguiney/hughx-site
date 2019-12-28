5b4b4f870c06a678b23efdeefc717b49ca9e9f82
Hugh Guiney <hugh@hughguiney.com>
2019-12-27 19:38:33 -0500

    Add Google Analytics

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -3,5 +3,59 @@
  *
  * See: https://www.gatsbyjs.org/docs/browser-apis/
  */
+const ReactGA = require( "react-ga" );
 
-// You can delete this file if you're not using it
+function deuglifyAnalyticsURL() {
+  if ( "history" in window ) {
+    window.history.replaceState( {}, document.title, window.location.href.replace( /(\?|&)utm_([^&]+)/gi, "" ).replace( "?&", "?" ).replace( /(&|\?)$/i, "" ) );
+  }
+}
+
+function isProduction() {
+  return /^https?:\/\/(?:www\.)?hughx\.(?:dev|com)/i.test( window.location.origin );
+}
+
+function isDevelopment() {
+  return !isProduction();
+}
+
+function isInternalTrafficLink() {
+  return /(?:\?|&)utm_source=internal\b/.test( window.location.search );
+}
+
+function isInternalTraffic() {
+  let isInternal = false;
+  let hasInternalTrafficCookie = false;
+
+  if ( "localStorage" in window ) {
+    hasInternalTrafficCookie = ( window.localStorage.getItem( "internalTraffic" ) === "true" );
+  }
+
+  isInternal = ( isDevelopment() || hasInternalTrafficCookie );
+
+  return isInternal;
+}
+
+function setInternalTrafficCookie() {
+  window.localStorage.setItem( "internalTraffic", "true" );
+}
+
+function handleInternalTraffic() {
+  if ( ( "localStorage" in window ) && isInternalTrafficLink() ) {
+    setInternalTrafficCookie();
+  }
+}
+
+exports.onClientEntry = () => {
+  ReactGA.initialize( "UA-23329936-1" );
+};
+
+exports.onRouteUpdate = ( { location } ) => {
+  handleInternalTraffic();
+
+  if ( !isInternalTraffic() ) {
+    ReactGA.pageview( location.pathname );
+  }
+
+  deuglifyAnalyticsURL();
+};
